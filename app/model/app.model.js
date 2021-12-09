65b11b54da447d529f58aefe6dc637ccae1b537c
Taner Tun <t_tun@msn.com>
2021-12-09 18:35:15 +0200

    api

---
+++ app/model/app.model.js
@@ -0,0 +1,9 @@
+
+// Define  model in Mongoose
+const mongoose = require("mongoose");
+
+const AppSchema = mongoose.Schema({
+  message: String,
+});
+
+module.exports = mongoose.model("App", AppSchema);
---
+++ server.js
@@ -0,0 +1,32 @@
+
+// setting up the server
+const express = require('express');
+const bodyParser = require('body-parser');
+// import mongoose
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+// connect to mongodb
+mongoose.connect("mongodb://127.0.0.1:27017/todo-mongoDB", {
+    useNewUrlParser: true
+}).then(() => {
+    console.log("Successfully connected to the database");    
+}).catch(err => {
+    console.log('Could not connect to the database. Error...', err);
+    process.exit();
+});
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.use(bodyParser.json())
+
+app.get('/', (req, res) => {
+    res.json({"message": "Server is running :D"});
+});
+
+let PORT = 8080
+require('./app/routes/app.routes.js')(app);
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+});
